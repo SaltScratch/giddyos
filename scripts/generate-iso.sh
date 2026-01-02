a6728a5a9f7d26bed02cbfc47f3bf7bcbe6998a3
Allan Cairns <allan@cairns.id.au>
2026-01-02 20:32:04 +0800

    switch to using registry image for iso

--- scripts/generate-iso.sh
+++ scripts/generate-iso.sh
@@ -1,1 +1,2 @@
-sudo bluebuild generate-iso --iso-name giddyos.iso recipe ../recipes/recipe.yml
+sudo bluebuild generate-iso --iso-name giddyos.iso image ghcr.io/saltscratch/giddyos
+#sudo bluebuild generate-iso --iso-name giddyos.iso recipe ../recipes/recipe.yml
